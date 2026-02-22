4eaddc20f78ecc929bc652600d97ec13197debcd
Ayush Kumar <ayushkumarofficework@gmail.com.com>
2026-02-22 14:03:13 +0530

    [experimented on tensors using numpy]

---
+++ basics/tensors_basics.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+a = np.array(1)
+
+
+a=np.array([1,2,3])
+a=np.array([[[[1,2,3],[45,66.2,6]],[[1,2,3],[45,66.2,6]]],[[[1,2,3],[45,66.2,6]],[[1,2,3],[45,66.2,6]]],[[[1,2,3],[45,66.2,6]],[[1,2,3],[45,66.2,6]]]])
+
+
+print(a)
+print(a.ndim)
+print(a.size)
+print(a.itemsize)
+print(a.dtype)
+print(a.shape)
